a6863ad0ffabcaa9712cc986dfbc172969d1dfe5
Beisembekov Mukhammedali <98638306+Mukhammedali22@users.noreply.github.com>
2024-07-26 23:28:44 +0500

    Done

---
+++ NumericalDifferentiationIntegration/mid.py
@@ -0,0 +1,21 @@
+from math import (pi, exp, sin, cos)
+
+def f(x):
+    return sin(x)
+
+def Midpoint(f, a, b, N):
+    """Midpoint rule for numerical integration"""
+    dx = (b - a) / (N - 1)
+
+    s = 0
+    for i in range(N):
+        x = a + i*dx
+        s += f(x + 0.5*dx) * dx
+    
+    return s
+
+a, b = map(eval, input("a, b = ").split(", "))
+N = int(input("Number of discretized points: "))
+
+print(Midpoint.__doc__)
+print(Midpoint(f, a, b, N))
