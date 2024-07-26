55a43b9a2f8aefa78664b4d021d2f4d982d848c7
Beisembekov Mukhammedali <98638306+Mukhammedali22@users.noreply.github.com>
2024-07-26 23:48:03 +0500

    Done

---
+++ NumericalDifferentiationIntegration/mid.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+#define pi 3.14159265358979323846
+
+double f(double x) {
+    return sin(x);
+}
+
+// Midpoint rule for numerical integration
+double Midpoint(double (*f)(double x), double a,
+                    double b, int N) {
+    double dx = (b - a) / (N - 1);
+    double x = a;
+
+    double s = 0;
+    for (int i = 0; i < N; ++i) {
+        x = a + i*dx;
+        s += f(x + 0.5*dx) * dx;
+    }
+
+    return s;
+}
+
+
+int main() {
+    double a, b;
+    std::cout << "[a b] = ";
+    std::cin >> a >> b;
+    int N;
+    std::cout << "Number of discretized points: ";
+    std::cin >> N;
+
+    double result = Midpoint(f, a, b, N);
+    std::cout << "Midpoint rule\n";
+    std::cout << result << "\n";
+    printf("%e\n", result);
+  
+    return 0;
+}
